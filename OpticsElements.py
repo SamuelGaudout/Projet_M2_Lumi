7b62ad6e288d28c27a573974e37c02a8ff57f8b1
SamuelGaudout <samuel.gaudout@etu.sorbonne-universite.fr>
2023-01-25 16:11:00 +0100

    collisions

--- OpticsElements.py
+++ OpticsElements.py
@@ -33,7 +33,26 @@ def findParticle(particles, x, y):
             return p
     return None
 
+def collide(p1, p2):
+    dx = p1.x - p2.x
+    dy = p1.y - p2.y
 
+    dist = math.hypot(dx, dy)
+    if dist < p1.size + p2.size:
+        tangent = math.atan2(dy, dx)
+        angle = 0.5 * math.pi + tangent
+
+        angle1 = 2*tangent - p1.angle
+        angle2 = 2*tangent - p2.angle
+        speed1 = p2.speed * elasticity
+        speed2 = p1.speed * elasticity
+
+        (p1.angle, p1.speed) = (angle1, speed1)
+        (p2.angle, p2.speed) = (angle2, speed2)
+        p1.x += math.sin(angle)
+        p1.y -= math.cos(angle)
+        p2.x -= math.sin(angle)
+        p2.y += math.cos(angle)
 
 
 class Particle:
@@ -126,10 +145,14 @@ while running:
 
 
     screen.fill(background_colour)
-    for particle in my_particles:
-        if particle != selected_particle:
-            particle.move()
-            particle.bounce()
+
+
+    for i,particle in enumerate(my_particles):
+        #if particle != selected_particle:
+        particle.move()
+        particle.bounce()
+        for particle2 in my_particles[i+1:]:
+            collide(particle, particle2)
         particle.display()
 
     
